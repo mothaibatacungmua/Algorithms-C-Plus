a04e71d28c881d134e05fd402149cf89d9a5d360
Duc Tran <duc20102638@gmail.com>
2016-04-27 02:34:45 +0700

    Add type parse for string and char*

---
+++ src/data-structures/hashmap.cpp
@@ -0,0 +1,16 @@
+/*
+ * hashmap.cpp
+ *
+ *  Created on: Apr 27, 2016
+ *      Author: asm
+ */
+#include <iostream>
+#include <string>
+#include "../../headers/type-parse.hpp"
+#include "../../headers/data-structures.hpp"
+
+
+using namespace DataStructures;
+
+
+
--- src/utils/type-parse.cpp
+++ src/utils/type-parse.cpp
@@ -7,21 +7,37 @@
 #include "../../headers/type-parse.hpp"
 #include <stdio.h>
 
+using namespace std;
+
 namespace Utils{
     template<typename T>
-    std::string ToString(T a){
+    string ToString(T a){
+        char buffer[16];
         if(COMPARE_TYPE(T,"int")){
-            char buffer[16];
             sprintf ( buffer, "%d", a );
             return buffer;
         }
 
+        if(COMPARE_TYPE(T,"double")){
+            sprintf( buffer, "%0.2f", a);
+            return buffer;
+        }
+
+        if(COMPARE_TYPE(T,"char*")){
+            return a;
+        }
+
+        if(COMPARE_TYPE(T,"string")){
+            return a;
+        }
         return "NOT IMPLEMENTED YET";
     }
 }
 
-template std::string Utils::ToString<int>(int);
-template std::string Utils::ToString<double>(double);
+template string Utils::ToString<int>(int);
+template string Utils::ToString<double>(double);
 
 REGISTER_PARSE_TYPE(int);
 REGISTER_PARSE_TYPE(double);
+REGISTER_PARSE_TYPE(char*);
+REGISTER_PARSE_TYPE(string);
